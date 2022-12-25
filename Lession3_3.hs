a649b94ab93bc3cbc8fd8f513323b8b71e7f8157
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-25 13:43:37 +0000

    Updated!

--- Lession3_3.hs
+++ Lession3_3.hs
@@ -6,6 +6,7 @@ portfolio b e =
 {-b là số bitcoin, e là số ethereum, c là số % bitcoin và d là số % ethereum dominance có thể check tại conmarketcap.com
     16822 $ là giá bitcoin và 1100 $ là giá ethereum ngày 25th Dec 2022-}
   let total b e c d = b * 16822 * c/100 + e * 1218 * d/100
+      c + d = 100
       alle e = e * 1100
       allb b = b * 16800
     in if total b e c d > alle e && total b e c d > allb b then "Let's bet by dominance method"
