844c746e9d46ccc48bef70585563a8c2e2c33a01
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-25 13:38:41 +0000

    Updated

--- Lession3_1.hs
+++ Lession3_1.hs
@@ -8,15 +8,3 @@ monthlyusage w h =
  let monthlyusage w h = w * h * 30  
    in if monthlyusage w h > 60 then "Over" 
 else "Normal"
-
--- Question 2
--- Prelude:
--- We use the function `show :: a -> String` to transform any type into a String.
--- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
-
--- In the previous function, return the excess/savings of consumption as part of the message.
-monthlyusage :: Int -> Int -> String
-monthlyusage w h = 
- let monthlyusage w h = w * h * 30  
-   in if monthlyusage w h > 60 then "Over"++ show ((monthlyusage w h) - 60)
-else "Normal"++ show (60 - (monthlyusage w h))
---
+++ Lession3_3.hs
@@ -0,0 +1,15 @@
+-- Question 3
+-- Write a function that showcases the advantages of using let expressions to split a big expression into smaller ones.
+-- Then, share it with other students in Canvas.
+portfolio :: Double -> Double -> Double -> Double -> Double -> String
+portfolio b e =
+{-b là số bitcoin, e là số ethereum, c là số % bitcoin và d là số % ethereum dominance có thể check tại conmarketcap.com
+    16822 $ là giá bitcoin và 1100 $ là giá ethereum ngày 25th Dec 2022-}
+  let total b e c d = b * 16822 * c/100 + e * 1218 * d/100
+      alle e = e * 1100
+      allb b = b * 16800
+    in if total b e c d > alle e && total b e c d > allb b then "Let's bet by dominance method"
+     else if alle e > total b e c d && alle e > allb b then "Let's be all in eth"
+  else "Let's be all in btc"
+      
+   
