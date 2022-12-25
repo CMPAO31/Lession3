0df9de8ed0a4764e080ce58f2e471dbec223502d
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-25 13:00:08 +0000

    Updated!

--- Lession3.hs
+++ Lession3.hs
@@ -3,7 +3,20 @@
 -- returns a message accordingly. 
 -- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
 -- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
-monthlyusage :: Double -> Double -> Double -> String
-monthlyusage consumption = 
- let consumption k h = k * h * 30  
-   in if consumption > 60 then "Over" else "Normal"
+monthlyusage :: Int -> Int -> String
+monthlyusage w h = 
+ let monthlyusage w h = w * h * 30  
+   in if monthlyusage w h > 60 then "Over" 
+else "Normal"
+
+-- Question 2
+-- Prelude:
+-- We use the function `show :: a -> String` to transform any type into a String.
+-- So `show 3` will produce `"3"` and `show (3 > 2)` will produce `"True"`.
+
+-- In the previous function, return the excess/savings of consumption as part of the message.
+monthlyusage :: Int -> Int -> String
+monthlyusage w h = 
+ let monthlyusage w h = w * h * 30  
+   in if monthlyusage w h > 60 then "Over"++ show ((monthlyusage w h) - 60)
+else "Normal"++ show (60 - (monthlyusage w h))
