bdb26421fb6a3d712a7afea0eb9df651c7be65ab
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-25 11:03:20 +0000

    Update

--- Lession3.hs
+++ Lession3.hs
@@ -3,11 +3,7 @@
 -- returns a message accordingly. 
 -- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
 -- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
-check :: Double->Double->String
-check p h 
-    |consumption>max ="Not allow"
-    |consumption<max ="Allow"
-    |consumption==max ="Allow"
-  where
-    consumption=p*h*30
-    max=60
+monthlyusage k h : Double->Double->String
+monthlyusage k h = k * h * 30
+ if monthlyusage>60 then "Not allow"
+ else "Allow"
