fc03144d79a8d9deb3f331551d8b9673614dfa91
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-26 14:31:03 +0000

    Updated

--- Lession3_5.hs
+++ Lession3_5.hs
@@ -4,4 +4,16 @@
 -- let expression inside a where block. 
 
 
-{-I don't have an answer :( -}
+{-Refer Mr.Hieu
+square root là căn bậc hai nhé. product= tích, quotient=thương.
+sqrt là hàm của square root
+the sum of squares for the product and quotient la'
+ (a.b)^2 + (a/b)^2-}
+f5 :: Double->Double-> String
+f5 a b =
+    let sqrProd =  xyProd^2 where xyProd = x * y
+    in  if y /= 0 then show ( sqrProd +  sqrQuot)
+        else "The division is not possible"
+    where
+        sqrQuot = let xyQuot = (/) x y
+                        in   xyQuot^2
